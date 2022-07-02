89a2e48443c7395988ad987434646d8b2dd2dc01
Alexandr Panchenko <alexandr.panchenko.93@yandex.ru>
2022-07-02 08:41:28 +0300

    clarify test intention

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -5,20 +5,69 @@ describe("User can", () => {
   const board = new Board();
   const rectA = board.addRect({ x: 10, y: 10, width: 10, height: 10 });
   const rectB = board.addRect({ x: 14, y: 14, width: 10, height: 10 });
-  const rectC = board.addRect({ x: 17, y: 17, width: 10, height: 10 });
+  const rectC = board.addRect({ x: 16, y: 16, width: 10, height: 10 });
+  const rectD = board.addRect({ x: 17, y: 17, width: 10, height: 10 });
+  const rectE = board.addRect({ x: 18, y: 18, width: 10, height: 10 });
   it("find rectangles under pointer", () => {
     assert.deepEqual(board.getAllUnderPoint({ x: 15, y: 15 }), [rectA, rectB]);
   });
   it("reorder rectangles", () => {
     board.moveFirstAfterSecond(rectA, rectB);
-    assert.deepEqual(board.getAllUnderPoint({ x: 15, y: 15 }), [rectB, rectA]);
+    assert.deepEqual(board.getAllUnderPoint({ x: 19, y: 19 }), [
+      rectB,
+      rectA,
+      rectC,
+      rectD,
+      rectE,
+    ]);
+    board.moveFirstAfterSecond(rectA, rectD);
+    assert.deepEqual(board.getAllUnderPoint({ x: 19, y: 19 }), [
+      rectB,
+      rectC,
+      rectD,
+      rectA,
+      rectE,
+    ]);
+    board.moveFirstAfterSecond(rectE, rectC);
+    assert.deepEqual(board.getAllUnderPoint({ x: 19, y: 19 }), [
+      rectB,
+      rectC,
+      rectE,
+      rectD,
+      rectA,
+    ]);
   });
   it("undo operation", () => {
     board.undo();
-    assert.deepEqual(board.getAllUnderPoint({ x: 18, y: 18 }), [
+    assert.deepEqual(board.getAllUnderPoint({ x: 19, y: 19 }), [
+      rectB,
+      rectC,
+      rectD,
+      rectA,
+      rectE,
+    ]);
+    board.undo();
+    assert.deepEqual(board.getAllUnderPoint({ x: 19, y: 19 }), [
+      rectB,
+      rectA,
+      rectC,
+      rectD,
+      rectE,
+    ]);
+    board.undo();
+    assert.deepEqual(board.getAllUnderPoint({ x: 19, y: 19 }), [
       rectA,
       rectB,
       rectC,
+      rectD,
+      rectE,
+    ]);
+    board.undo();
+    assert.deepEqual(board.getAllUnderPoint({ x: 19, y: 19 }), [
+      rectA,
+      rectB,
+      rectC,
+      rectD,
     ]);
   });
 });
